05dcd44f0d5a4e600312432f81d377aadafaed45
Vivian Zhang <vivzhang@mit.edu>
2017-09-16 15:15:52 -0400

    Add pic to div

--- content.js
+++ content.js
@@ -4,15 +4,16 @@ document.documentElement.style.width = '100%';
 document.body.style.width = '100%';
 
 var div = document.createElement( 'div' );
-var btnForm = document.createElement( 'form' );
-var btn = document.createElement( 'input' );
+var img = document.createElement('img');
+
+img.src = 'https://i.ytimg.com/vi/2qZHh_iN5Zs/hqdefault.jpg'
 
 //append all elements
 document.body.appendChild( div );
-div.appendChild( btnForm );
-btnForm.appendChild( btn );
+div.appendChild(img);
+
 //set attributes for div
-div.id = 'myDivId';
+div.id = 'blackout';
 div.style.zindex = '100000000';
 div.style.position = 'fixed';
 div.style.top = '0%';
@@ -23,5 +24,5 @@ div.style.backgroundColor = 'black';
 
 setTimeout(function() {
 	document.body.removeChild(div);
-}, 1000)
+}, 2000)
 
