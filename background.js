2bdd49b688dffe1752665448fa2fe19662a6029f
szabom <szabom@mit.edu>
2017-09-16 14:13:07 -0400

    blocks multiple sites!

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// When the extension is installed or upgraded ...
+
+//
+blocked_sites = 'facebook|twitter'
 chrome.runtime.onInstalled.addListener(function() {
   // Replace all rules ...
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     // With a new rule ...
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        // That fires when a page's URL contains 'facebook' ...
+        // That fires when a page's URL contains anything in blocked_sites ...
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'facebook' },
+            pageUrl: { urlMatches: blocked_sites+'|google'},
           })
         ],
         // And shows the extension's page action.
